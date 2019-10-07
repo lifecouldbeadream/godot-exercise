63cfec22390d343961fdc335699d905046ab0cd6
horraline <layufeng@gmail.com>
2019-10-08 00:23:27 +0800

    no message

--- player.gd
+++ player.gd
@@ -1,13 +1,30 @@
 extends Area2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (int) var speed
+var screensize
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screensize = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2();
+	if Input.is_action_just_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y += 1
+	if Input.is_action_just_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_just_pressed("ui_down"):
+		velocity.y -= 1
+		
+	if velocity.length() >0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x,0,screensize.x)
+	position.y = clamp(position.y,0,screensize.y)
+		
